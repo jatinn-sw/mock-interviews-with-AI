f56717d9fe64b67fbbfc4baf0063dd65c8ed107a
Jatin Kumar Sangewar <sangewarjatinkumar@gmail.com>
2026-02-11 21:21:45 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
+  experimental: {
+    turbopack: {
+      resolveAlias: {},
+    },
+  },
 };
 
 export default nextConfig;
